aa443237b1e0a697dd0161d72c643c1f1d3163e8
rhbeltran <119127029+rhbeltran@users.noreply.github.com>
2023-11-24 20:38:16 -0500

    Update App.js cambio idioma
    Cambiar a español y agregar http de puce

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EDITAR <code>src/App.js</code> GRABAR Y MOSTRAR EN PAGINA :).
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wwww.puce.edu.ec"
           target="_blank"
           rel="noopener noreferrer"
         >
