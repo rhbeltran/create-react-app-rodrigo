9a8a28e273e507b19c8861c013d940cef71d1836
rhbeltran <119127029+rhbeltran@users.noreply.github.com>
2023-11-24 20:47:42 -0500

    Update App.js imagen 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://pbs.twimg.com/media/FNNVF4TX0BI_SJw.jpg" className="App-logo" alt="logo" />
         <p>
           EDITAR <code>src/App.js</code> GRABAR Y MOSTRAR EN PAGINA :).
         </p>
