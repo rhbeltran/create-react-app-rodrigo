aacc8806bc676dfe5dbd93334f2d7b83d7a49565
rhbeltran <119127029+rhbeltran@users.noreply.github.com>
2023-11-24 20:40:05 -0500

    Update App.js correo
    correxion de http

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://wwww.puce.edu.ec"
+          href="https://www.puce.edu.ec"
           target="_blank"
           rel="noopener noreferrer"
         >
